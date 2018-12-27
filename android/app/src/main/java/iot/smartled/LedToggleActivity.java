86e497c2788fd9a34986e66c740a22fbf3a37ccd
Prashanth Ravoor <prash.ravoor125@gmail.com>
2018-12-28 00:27:25 +0530

    Added android project, UI mostly broken though

---
+++ android/app/src/main/java/iot/smartled/LedToggleActivity.java
@@ -0,0 +1,13 @@
+package iot.smartled;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LedToggleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_led_toggle);
+    }
+}
